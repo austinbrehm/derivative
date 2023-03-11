1bb94b7f22b246dba4f7f066d6a102b1143b4bed
Austin Brehm <austin.brehm@gmail.com>
2023-03-10 18:07:39 -0700

    plot tangent line and point of interest

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot(x, y, x_derivative, y_derivative, title, x_label, y_label):
+def plot(x, y, x_derivative, y_derivative, x_point, y_point, x_tangent, y_tangent, title, x_label, y_label):
     plt.style.use('dark_background')
-    plt.plot(x, y, 'fuchsia', label='f(x) = x**2', linewidth=1)
-    plt.plot(x_derivative, y_derivative, 'lime', label='f\'(x) = 2*x', linewidth=1)
+    plt.plot(x, y, 'fuchsia', label='f(x) = x^2', linewidth=2)
+    plt.plot(x_derivative, y_derivative, 'lime', label='f\'(x) = 2x', linewidth=2)
+    plt.plot(x_point, y_point, marker='o', c='red', label='point of interest')
+    plt.plot(x_tangent, y_tangent, linestyle='dashed', c='snow', label='tangent line', linewidth=1)
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
@@ -14,14 +16,21 @@ def plot(x, y, x_derivative, y_derivative, title, x_label, y_label):
     plt.show()
 
 
-# create data points
+# create f(x) data points
 x_data = np.linspace(-100, 100, num=1000)
 y_data = x_data**2
 
-# create derivative data points
+# create f'(x) data points
 x_derivative_data = np.linspace(-100, 100, num=1000)
 y_derivative_data = 2*x_data
 
+# create tangent line data points
+# equation of tangent line: y = m(x-x1) + y1
+x1 = 50
+y1 = x1**2
+x_tangent_data = np.linspace(30, 70, num=1000)
+y_tangent_data = (2*x1)*(x_tangent_data - x1) + y1
+
 # plot
-plot(x_data, y_data, x_derivative_data, y_derivative_data, 'Derivatives', 'x', 'y')
+plot(x_data, y_data, x_derivative_data, y_derivative_data, x1, y1, x_tangent_data, y_tangent_data, 'Derivatives', 'x', 'y')
 
